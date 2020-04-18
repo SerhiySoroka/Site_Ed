2fbc1f02c50351c125df8e740bd4b70066df2cb7
Serhiy Poluhtovych <45402580+SerhiySoroka@users.noreply.github.com>
2020-04-18 23:08:08 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,66 @@
+jQuery(function($) {'use strict',
+	
+	 $("#countdown").countdown({
+			date: "10 july 2014 12:00:00",
+			format: "on"
+		},
+		
+		function() {
+		});
+	
+
+	
+	//Меню прокручування
+
+	function menuToggle()
+	{
+		var windowWidth = $(window).width();
+
+		if(windowWidth > 767 ){
+			$(window).on('scroll', function(){
+				if( $(window).scrollTop()>405 ){
+					$('.main-nav').addClass('fixed-menu animated slideInDown');
+				} else {
+					$('.main-nav').removeClass('fixed-menu animated slideInDown');
+				}
+			});
+		}else{
+			
+			$('.main-nav').addClass('fixed-menu animated slideInDown');
+				
+		}
+	}
+
+	menuToggle();
+	
+	
+	// Автомтична змніа слайдів
+	$('#event-carousel, #twitter-feed, #sponsor-carousel ').carousel({
+		interval: false
+	});
+
+
+	// Перевірка контактної форми
+	var form = $('.contact-form');
+	form.submit(function () {'use strict',
+		$this = $(this);
+		$.post($(this).attr('action'), function(data) {
+			$this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
+		},'json');
+		return false;
+	});
+
+	$( window ).resize(function() {
+		menuToggle();
+	});
+
+	$('.main-nav ul').onePageNav({
+		currentClass: 'active',
+	    changeHash: false,
+	    scrollSpeed: 900,
+	    scrollOffset: 0,
+	    scrollThreshold: 0.3,
+	    filter: ':not(.no-scroll)'
+	});
+
+});
